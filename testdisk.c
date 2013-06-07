b83c54ace3d7ba18ede1a30b0a78fd7ff75f203f
Kyle <kparent@ucsc.edu>
2013-06-07 16:40:03 -0700

    changed data size

--- testdisk.c
+++ testdisk.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include "mydisk.h"
 #include "filesys.h"
-#define LINE_SIZE 84
+
 
 void copy_dir (disk_t disk, superblock sb, char *filename) {
   // Open file for reading
@@ -12,11 +12,12 @@ void copy_dir (disk_t disk, superblock sb, char *filename) {
     fprintf(stderr, "%s: bad filename.\n", filename);
   }
   // Determine size of file
+  // add move pointer to beginning of file
   fseek(infile, 0, SEEK_END);
   long f_size = ftell(infile);
   fseek(infile, 0, SEEK_SET);
   // Copy contents to buffer and write to disk
-  char *line = malloc(LINE_SIZE);
-  fread(line, 1, f_size, infile);
-  write_string_file(disk, sb, filename, line);
+  char *data = malloc(f_size);
+  fread(data, 1, f_size, infile);
+  write_string_file(disk, sb, filename, data);
 }
